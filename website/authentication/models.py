5d6c708c533ef7e91f5e0e95f503c4c5cd8ea2bc
MalesSebastian <malessebastian@gmail.com>
2016-11-30 18:44:01 +0200

    Authentication model

--- website/authentication/models.py
+++ website/authentication/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    __access_level__dict = {
+        1 : "Inspector",
+        2 : "Director"
+    }
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_level = models.IntegerField(default=-1)
+
+    def get_access_level(self):
+        return self.__statut_dict.get(self.statut, default="Unknown")
+
+    def validate_acces_level(self):
+        return self.get_acces_lever != "Unknown"
