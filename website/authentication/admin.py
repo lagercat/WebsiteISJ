4d61420d90647d19ae31c84770015a2766efc29f
MalesSebastian <malessebastian@gmail.com>
2016-12-01 00:55:53 +0200

    Model account added to admin

--- website/authentication/admin.py
+++ website/authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Account
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    ordering = ['user']
+
+admin.site.register(Account, AccountAdmin)
--- website/authentication/models.py
+++ website/authentication/models.py
@@ -12,9 +12,11 @@ class Account(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     access_level = models.IntegerField(default=-1)
+    # should make a school model
+    # should make a subject model
 
     def get_access_level(self):
-        return self.__statut_dict.get(self.statut, default="Unknown")
+        return self.__statut_dict.get(self.access_level, default="Unknown")
 
-    def validate_acces_level(self):
-        return self.get_acces_lever != "Unknown"
+    def validate_access_level(self):x
+        return self.get_access_level() != "Unknown"
