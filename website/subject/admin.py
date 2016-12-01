c1f08172e080fc0888f555e11ecbad20e043e146
MalesSebastian <malessebastian@gmail.com>
2016-12-01 21:44:35 +0200

    Add subject to admin panel

--- website/subject/admin.py
+++ website/subject/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Subject
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+
+admin.site.register(Subject, SubjectAdmin)
