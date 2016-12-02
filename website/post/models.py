1709a70ea393784f1fa5af6db6c81ec07f6d6f56
MalesSebastian <malessebastian@gmail.com>
2016-12-02 21:59:02 +0200

    Add date field to post model

--- website/post/models.py
+++ website/post/models.py
@@ -8,9 +8,11 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, related_name='posts', blank=False)
     file = models.FileField()
+    date = models.DateTimeField(Editable=False, auto_now_add=True, blank=False)
 
+    @property
     def filename(self):
         return os.path.basename(self.file)
 
     def __unicode__(self):
-        return "File %s from %s" % (self.filename(), self.author.username)
+        return "File %s from %s" % (self.filename, self.author.username)
