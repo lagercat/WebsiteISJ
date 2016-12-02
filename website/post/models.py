19a1aa390df1834f5c6ecd7661d6ac06160b28b0
MalesSebastian <malessebastian@gmail.com>
2016-12-02 21:52:32 +0200

    Add post model

--- website/post/models.py
+++ website/post/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
+import os
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='posts', blank=False)
+    file = models.FileField()
+
+    def filename(self):
+        return os.path.basename(self.file)
+
+    def __unicode__(self):
+        return "File %s from %s" % (self.filename(), self.author.username)
