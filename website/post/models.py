8ea1afa92d16cb9c118b73ee7aadc6b6f617ea13
MalesSebastian <malessebastian@gmail.com>
2016-12-03 13:04:25 +0200

    Add name field to post model

--- website/post/models.py
+++ website/post/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, related_name='posts', blank=False)
+    name = models.CharField(max_length=100, blank=False, null=True)
     file = models.FileField()
     date = models.DateTimeField(editable=False, auto_now_add=True, blank=False, null=True)
 
