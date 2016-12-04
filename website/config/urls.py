4985241ce2b346c75665b8a61bfa28a2697773cb
MalesSebastian <malessebastian@gmail.com>
2016-12-04 14:59:30 +0200

    Fix filename function from post model

--- website/config/urls.py
+++ website/config/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('homepages.urls')),
-    url(r'^', include('authentication.urls'))
+    url(r'^', include('authentication.urls')),
+    url(r'^', include('post.urls'))
 ]
