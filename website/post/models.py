5405919f1c8ea8ea9e7787f03e26d030f5d9e627
MalesSebastian <malessebastian@gmail.com>
2016-12-04 15:21:05 +0200

    Add upload file location

--- website/post/models.py
+++ website/post/models.py
@@ -5,15 +5,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def user_directory_path(instance, filename):
+    return 'documents/user_{0}/{1}'.format(instance.author.username, filename)
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, related_name='posts', blank=False)
     name = models.CharField(max_length=100, blank=False, null=True)
-    file = models.FileField()
+    file = models.FileField(upload_to=user_directory_path)
     date = models.DateTimeField(editable=False, auto_now_add=True, blank=False, null=True)
 
     @property
     def filename(self):
-        return os.path.basename(self.file)
+        return os.path.basename(self.file.url)
 
     def __unicode__(self):
         return "File %s from %s" % (self.filename, self.author.username)
