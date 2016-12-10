26a75f61d769c16a3290cb0ef8f251ed6acbbead
MalesSebastian <malessebastian@gmail.com>
2016-12-11 01:47:55 +0200

    Add index page

--- src/homepages/views.py
+++ src/homepages/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render_to_response
-from django.template import RequestContext
 
-
-# Create your views here.
 
 def home(request):
     if request.user.is_authenticated():
         template = 'homepages/home.html'
     else:
         template = 'homepages/index.html'
-    return render_to_response(template)
+    return render_to_response(template, {'user': request.user})
