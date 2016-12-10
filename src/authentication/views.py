bda79a252b8c6e3cc712101a36a4eecdb6a768f5
MalesSebastian <malessebastian@gmail.com>
2016-12-10 23:22:44 +0200

    Add login view and template

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+
+from forms import LoginForm
 
 
 @login_required
@@ -8,3 +10,22 @@ def logout_view(request):
     logout(request)
     return redirect('/')
 
+
+def login_view(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+    form = LoginForm(request.POST)
+    errors = []
+    if request.method == "POST":
+        if form.is_valid():
+            user = authenticate(user=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user:
+                login(request, user)
+                return redirect('/')
+            else:
+                errors.append("Nume de utilizator sau parola invalida")
+    return render(request, 'authentication/login.html', {
+        'errors': errors,
+        'form': form
+    })
