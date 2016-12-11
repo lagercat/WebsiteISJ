8ff64bbd293f21b6479d7e58238d0623353759ca
MalesSebastian <malessebastian@gmail.com>
2016-12-11 02:42:38 +0200

    Upload file form

--- src/post/forms.py
+++ src/post/forms.py
@@ -9,7 +9,7 @@ class CreatePostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['file']
+        fields = ['name', 'file']
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
@@ -17,15 +17,15 @@ class CreatePostForm(forms.ModelForm):
 
     def clean_file(self):
         uploaded_file = self.cleaned_data['file']
-        max_size = 10  # We should discuss the maximum file size
+        max_size = 1000000  # 10 MB
         filename, file_type = os.path.splitext(uploaded_file.name)
         allowed_file_types = [
-            'doc', 'docx', 'docm', 'xls', 'xlsx', 'ppt',
-            'ppt', 'pps', 'zip', 'rar', 'jpg', 'jpg', 'jpeg'
-            'png', 'gif', 'bmp', 'txt', 'tif', 'rtf', 'pdf',
-            'odt', 'ace', 'ods', 'odg'
+            '.doc', '.docx', '.docm', '.xls', '.xlsx', '.ppt',
+            '.ppt', '.pps', '.zip', '.rar', '.jpg', '.jpeg'
+            '.png', '.gif', '.bmp', '.txt', '.tif', '.rtf', '.pdf',
+            '.odt', '.ace', '.ods', '.odg'
         ]
-        if file_type not in allowed_file_types:
+        if not(any(file_type in type for type in allowed_file_types)):
             raise forms.ValidationError("Fisierul nu se incadreaza in extensile permise")
         if uploaded_file.size > max_size:
             raise forms.ValidationError("Fisierul trece de dimensiunea maxima de %d" % max_size)
--- src/post/views.py
+++ src/post/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from forms import CreatePostForm
+
+
+@login_required
+def upload_file_form(request):
+    form = CreatePostForm(request.POST or None, request.FILES or None, user=request.user)
+    confirm = []
+    if request.method == 'POST':
+        if form.is_valid():
+            post = form.instance
+            post.author = request.user
+            post.save()
+            confirm.append("Fisierul a fost incarcat")
+    print form.errors
+    return render(request, "post/form.html", {
+        "form": form,
+        "confirm": confirm})
+
