3a6dff107d4bbdb18f958e18a7cf4d059d350513
adriangaro <garovat.adrian@gmail.com>
2017-01-07 16:14:46 +0200

    Created new user class, new user manager, and user admin forms. Adde material theme for admin and forms.

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from material.frontend import urls as frontend_urls
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('homepages.urls')),
-    url(r'^', include('authentication.urls')),
-    url(r'^', include('post.urls'))
+  url(r'', include(frontend_urls)),
+  url(r'^admin/', admin.site.urls),
+  url(r'^', include('homepages.urls')),
+  url(r'^', include('authentication.urls')),
+  url(r'^', include('post.urls'))
 ]
