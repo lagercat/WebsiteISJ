1f7321e454b94caf97b1717214742a84da3ce3c4
MalesSebastian <malessebastian@gmail.com>
2017-01-08 23:11:04 +0200

    Fix status parameter in create_user

--- src/authentication/migrations/0002_extendeduser_subjects.py
+++ src/authentication/migrations/0002_extendeduser_subjects.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('authentication', '0001_initial'),
+        ('subject', '0001_initial'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='School',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100, null=True)),
-            ],
+        migrations.AddField(
+            model_name='extendeduser',
+            name='subjects',
+            field=models.ManyToManyField(blank=True, to='subject.Subject'),
         ),
     ]
