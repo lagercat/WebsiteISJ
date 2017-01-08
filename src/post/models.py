b8d4fc6938aad20a1751faa69e6bf2e9fa50632b
MalesSebastian <malessebastian@gmail.com>
2017-01-08 15:41:58 +0200

    Add time filter for interior posts

--- src/post/models.py
+++ src/post/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, related_name='posts', blank=False)
     name = models.CharField(max_length=100, blank=False, null=True)
     file = models.FileField(upload_to=user_directory_path)
-    date = models.DateTimeField(editable=False, auto_now_add=True, blank=False, null=True)
+    date = models.DateTimeField(auto_now_add=True, editable=False, blank=False, null=True)
     slug = models.SlugField(default=uuid.uuid1, unique=True)
 
     @property
