717179c4a46763432693c3284eadd578203d6ab0
MalesSebastian <malessebastian@gmail.com>
2017-01-10 10:49:15 +0200

    Add Subject form view

--- src/subject/views.py
+++ src/subject/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from forms import SubjectPostCreationForm
+
+
+@login_required
+def create_subject_post(request):
+    if request.user.status != 2:
+        raise HttpResponseForbidden
+    form = SubjectPostCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return render("subject/subject_post.html", {
+        "form": form
+    })
