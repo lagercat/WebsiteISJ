4561067f972d411a79e2ada09287ffb489c2232f
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-07 14:30:47 +0200

    views.py  news

--- src/news/urls.py
+++ src/news/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'news', views.news, name='news'),
+    url(r'^news/(?P<slug>[^\.]+)/$', views.news, name='news'),
 ]
--- src/news/views.py
+++ src/news/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+from models import News
 
 
 # Create your views here.
 
-def news(request):
-    return render(request, 'homepages/articol.html')
+def news(request, slug):
+    articol = get_object_or_404(News, slug=slug)
+    return render(request, 'homepages/articol.html',
+                  {
+                      'news': articol,
+                  })
