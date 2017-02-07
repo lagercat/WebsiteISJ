494547a3d43eedc4c7dc04fcd7b8cbdce53ec872
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-07 14:48:42 +0200

    views.py in news app for other news

--- src/news/views.py
+++ src/news/views.py
@@ -9,7 +9,9 @@ from models import News
 
 def news(request, slug):
     articol = get_object_or_404(News, slug=slug)
+    other_news = News.objects.all()[:4]
     return render(request, 'homepages/articol.html',
                   {
                       'news': articol,
+                      'other_news': other_news,
                   })
