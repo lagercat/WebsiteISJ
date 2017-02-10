89d8fb1464f872a02fab7d66166b8db0f7aae109
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-10 21:39:50 +0200

    event date time field fix

--- src/event/forms.py
+++ src/event/forms.py
@@ -3,17 +3,22 @@ Created on Jan 10, 2017
 
 @author: roadd
 '''
+from django.forms import SplitDateTimeWidget
 from event.models import Event
 from django import forms
 
+
 class EventCreationFormAdmin(forms.ModelForm):
+    time = forms.SplitDateTimeField()
+
     class Meta:
         model = Event
-        fields = ('title', 'description', 'image', 'time', 'location')
+        fields = ('title', 'description', 'image', 'location')
 
     def save(self, commit=True):
         event = super(EventCreationFormAdmin, self).save(commit=False)
         event.author = self.current_user
+        event.time = self.cleaned_data['time']
         if commit:
             event.save()
         return event
