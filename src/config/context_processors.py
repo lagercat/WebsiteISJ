d3de8a5da59d767afe2af5fcb4b69353756c76d6
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-19 00:12:03 +0200

    categories in right of page

--- src/config/context_processors.py
+++ src/config/context_processors.py
@@ -1,7 +1,9 @@
-from page.models import Category
+from subject.models import Subject
+from page.models import Category, Subcategory, SimplePage
 
 
 def template_context(request):
+
     return {
         'categories': Category.objects.all(),
     }
