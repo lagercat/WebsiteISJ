874bb8f8e9049e63649882cf4c4c55e531072202
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-18 21:54:41 +0200

    simple plage

--- src/config/context_processors.py
+++ src/config/context_processors.py
@@ -1,6 +1,6 @@
 
 from subject.models import Subject
-from page.models import Category, Subcategory
+from page.models import Category, Subcategory,SimplePage
 
 
 def template_context(request):
@@ -11,8 +11,15 @@ def template_context(request):
             Subcategory.objects.all().filter(category=value).order_by(
                 "name").values("name", ))[:7] for value in category
         }
+    simple_page = {
+        value.title: list(
+            SimplePage.objects.all().filter(category=value).order_by(
+                "name").values("name", ))[:7] for value in category
+        }
+    print header.items()
     return {
 
         'header': header.items(),
+        'simple_page': simple_page.items(),
         'subjects': subjects,
      }
