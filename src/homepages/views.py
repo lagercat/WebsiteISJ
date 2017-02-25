2274c8e2ebf2e0efcca1e008e487ff2397fcc716
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-25 22:51:23 +0200

    schools in index page

--- src/homepages/views.py
+++ src/homepages/views.py
@@ -3,6 +3,7 @@ from news.models import News
 from event.models import Event
 from subject.models import Subject
 from gallery.models import Gallery
+from school.models import School
 from page.models import Category, Subcategory
 from django.template import RequestContext
 
@@ -13,9 +14,11 @@ def home(request):
     events = Event.objects.all()[:9]
     subjects = Subject.objects.all()
     album = Gallery.objects.all()[:3]
+    schools = School.objects.all()[:3]
     return render(request, template, {
         'noutati': noutati,
         'events': events,
         'subjects': subjects,
         'album': album,
+        'schools':schools,
     })
--- src/school/views.py
+++ src/school/views.py
@@ -20,7 +20,7 @@ def schools(request, slug):
         School.objects.values('name', 'telephone', 'fax', 'email', 'website',
                               'address',
                               'geolocation', 'file').filter(slug=slug))
-    other_schools = School.objects.all()[:4]
+    other_schools = School.objects.all()[:3]
     return render(request, 'school/school_detail.html', {
         'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
         'name': articol[0].get('name'),
