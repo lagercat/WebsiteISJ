bbd708c078516a0d0e4cd599b35726408b2e05bd
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-25 17:54:13 +0200

    models for school

--- src/school/models.py
+++ src/school/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from utility.models import CustomPermissionsMixin
+from django_google_maps import fields as map_fields
 
 
 class School(CustomPermissionsMixin):
@@ -10,7 +11,14 @@ class School(CustomPermissionsMixin):
         verbose_name = "School"
         verbose_name_plural = "Schools"
         index_text = "Manage"
+
     name = models.CharField(max_length=100, null=True)
+    telephone = models.CharField(max_length=11, null=True)
+    fax = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(max_length=20, blank=True)
+    website = models.CharField(max_length=100, blank=True)
+    address = map_fields.AddressField(max_length=200)
+    geolocation = map_fields.GeoLocationField(max_length=100)
 
     def __unicode__(self):
         return self.name
