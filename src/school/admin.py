79e44609506aa125a8d1170206d5f5bc5f9875dc
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-25 18:45:39 +0200

    school netwoork

--- src/school/admin.py
+++ src/school/admin.py
@@ -1,12 +1,56 @@
 from django.contrib import admin
-
 from models import School
 from utility.admin import AdminChangeMixin, register_model_admin
+from django import forms
 
+from forms import SchoolCreationFormAdmin, SchoolChangeFormAdmin
+from django.contrib.admin.filters import DateFieldListFilter
+
+
+# Register your models here.
 
 class SchoolAdmin(AdminChangeMixin):
-    list_display = ['name']
-    list_order = ['name']
-    icon = '<i class="material-icons">location_city</i>'
+    add_form = SchoolCreationFormAdmin
+    change_form = SchoolChangeFormAdmin
+
+    icon = '<i class="material-icons">room</i>'
+
+    list_display = ('name', 'telephone', 'fax', 'email', 'website', 'address',
+                  'geolocation', 'file',)
+
+    search_fields = (
+        'name', 'address',)
+
+    filter_horizontal = ()
+
+    change_fieldsets = (
+        ('School name', {'fields': ('name',)}),
+        ('School Contact',
+         {'fields': ('telephone', 'fax', 'email', 'website', 'file',)}),
+        ('Location and Time', {'fields': ('address', 'geolocation',)}),
+    )
+
+    add_fieldsets = (
+        ('School name', {'fields': ('name',)}),
+        ('School Contact',
+         {'fields': ('telephone', 'fax', 'email', 'website', 'file',)}),
+        ('Location and Time', {'fields': ( 'address', 'geolocation',)}),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            self.fieldsets = self.add_fieldsets
+            form = self.add_form
+            form.current_user = request.user
+            return form
+        else:
+            self.fieldsets = self.change_fieldsets
+            form = self.change_form
+            form.address_initial = obj.address
+            form.geolocation_initial = obj.geolocation
+            return form
+
+    pass
+
 
 register_model_admin(School, SchoolAdmin)
---
+++ src/school/forms.py
@@ -0,0 +1,91 @@
+from models import School
+from django import forms
+from tinymce.widgets import AdminTinyMCE
+import os
+from django_google_maps import fields as map_fields
+from django_google_maps import widgets as map_widgets
+from utility.utility import clean_file
+
+
+class SchoolCreationFormAdmin(forms.ModelForm):
+    address = forms.CharField(widget=map_widgets.GoogleMapsAddressWidget())
+    geolocation = forms.CharField()
+
+    class Meta:
+        model = School
+        fields = ('name', 'telephone', 'fax', 'email', 'website', 'address',
+                  'geolocation', 'file',)
+
+    def clean(self):
+        cleaned_data = super(SchoolCreationFormAdmin, self).clean()
+        geoloc = cleaned_data['geolocation']
+        addr = cleaned_data['address']
+        if geoloc == "Invalid address or no results":
+            self.add_error("address",
+                           forms.ValidationError("The address is invalid"))
+        if addr == "Invalid geolocation":
+            self.add_error("geolocation",
+                           forms.ValidationError("The geolocation is invalid"))
+        return cleaned_data
+
+    def clean_file(self):
+        uploaded_file = self.cleaned_data['file']
+        error = clean_file(uploaded_file, image=True)
+        if error:
+            raise forms.ValidationError(error)
+        return uploaded_file
+
+    def save(self, commit=True):
+        uploaded_file = super(SchoolCreationFormAdmin, self).save(commit=False)
+        uploaded_file.author = self.current_user
+        uploaded_file.address = self.cleaned_data['address']
+        uploaded_file.geolocation = self.cleaned_data['geolocation']
+        if commit:
+            uploaded_file.save()
+        return uploaded_file
+
+
+class SchoolChangeFormAdmin(forms.ModelForm):
+    address = forms.CharField(widget=map_widgets.GoogleMapsAddressWidget)
+    geolocation = forms.CharField()
+
+    class Meta:
+        model = School
+        fields = ('name', 'telephone', 'fax', 'email', 'website', 'address',
+                  'geolocation', 'file',)
+
+    def __init__(self, *args, **kwargs):
+        initial = {
+            'address': self.address_initial,
+            'geolocation': self.geolocation_initial,
+        }
+        kwargs['initial'] = initial
+        super(SchoolChangeFormAdmin, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super(SchoolChangeFormAdmin, self).clean()
+        geoloc = cleaned_data['geolocation']
+        addr = cleaned_data['address']
+        if geoloc == "Invalid address or no results":
+            self.add_error("address",
+                           forms.ValidationError("The address is invalid"))
+        if addr == "Invalid geolocation":
+            self.add_error("geolocation",
+                           forms.ValidationError("The geolocation is invalid"))
+        return cleaned_data
+
+    def clean_file(self):
+
+        uploaded_file = self.cleaned_data['file']
+        error = clean_file(uploaded_file, image=True)
+        if error:
+            raise forms.ValidationError(error)
+        return uploaded_file
+
+    def save(self, commit=True):
+        uploaded_file = super(SchoolChangeFormAdmin, self).save(commit=False)
+        uploaded_file.address = self.cleaned_data['address']
+        uploaded_file.geolocation = self.cleaned_data['geolocation']
+        if commit:
+            uploaded_file.save()
+        return uploaded_file
