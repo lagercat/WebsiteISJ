7e8007646497eaf843b1b8fba46579dbebd64ed3
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-02-25 22:11:33 +0200

    views detail school

--- src/school/urls.py
+++ src/school/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
 
+    url(r'^schools_detail/(?P<slug>[^\.]+)/$', views.school_detail, name='school_detail'),
     url(r'^schools', views.school_all, name='schools'),
+
 
     ]
--- src/school/views.py
+++ src/school/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
-
 from models import School
+from news.models import News
+from config import settings
 
 
 # Create your views here.
@@ -10,4 +11,25 @@ def school_all(request):
     school = School.objects.all()
     return render(request, 'school/school_all.html', {
         'news_all': school,
+    })
+
+
+def school_detail(request, slug):
+    articol = list(
+        School.objects.values('name', 'telephone', 'fax', 'email', 'website',
+                              'address',
+                              'geolocation', 'file').filter(slug=slug))
+    other_news = News.objects.all()[:4]
+    return render(request, 'school/school_detail.html', {
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+        'name': articol[0].get('name'),
+        'telephone': articol[0].get('telephone'),
+        'fax': articol[0].get('fax'),
+        'email': articol[0].get('email'),
+        'website': articol[0].get('website'),
+        'location': articol[0].get('address'),
+        'geolocation': articol[0].get('geolocation'),
+        'other_news': other_news,
+        'thumbnail': "/media/" + articol[0].get('file'),
+
     })
