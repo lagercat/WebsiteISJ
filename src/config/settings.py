b93e5cc0ca2e42caa4f54320bea49b4de7d1ea1a
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-04-26 15:47:20 +0300

    host in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -16,10 +16,10 @@ GOOGLE_MAPS_API_KEY = 'AIzaSyAaQMDSvMtqNgJCK9b9TwywchYETHCo_4g'
 
 DEBUG = True
 
-# ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "isj.tm.edu.ro", "isj.tm.edu.ro:8080",
-#                  "www.isj.tm.edu.ro", "www.isj.tm.edu.ro:8080",
-#                  "217.73.174.42", "217.73.174.42:8080"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "isj.tm.edu.ro", "isj.tm.edu.ro:8080",
+                 "www.isj.tm.edu.ro",
+                 "217.73.174.42", "217.73.174.42:8080"]
+
 
 
 INSTALLED_APPS = [
