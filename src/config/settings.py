158a55f795cfe768f29a09ee6dd31b3ff92a31f1
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-04-27 15:15:37 +0300

    host update

--- src/config/settings.py
+++ src/config/settings.py
@@ -17,10 +17,8 @@ GOOGLE_MAPS_API_KEY = 'AIzaSyAaQMDSvMtqNgJCK9b9TwywchYETHCo_4g'
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "isj.tm.edu.ro", "isj.tm.edu.ro:8080",
-                 "www.isj.tm.edu.ro","*.edu.ro",
-                 "217.73.174.42", "217.73.174.42:8080",]
-
-
+                 "www.isj.tm.edu.ro", "new.isj.tm.edu.ro", "192.168.1.248",
+                 "217.73.174.42", "217.73.174.42:8080"]
 
 INSTALLED_APPS = [
     # admin theme
