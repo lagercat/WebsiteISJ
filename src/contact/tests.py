6ceb5cbb24421a791163a23c686c37cb51d156aa
MalesSebastian <malessebastian@gmail.com>
2017-05-06 00:13:16 +0300

    Add comments for explaining test functions

--- src/contact/tests.py
+++ src/contact/tests.py
@@ -1,17 +1,20 @@
 from django.test import TestCase
 from contact.models import Contact
 
+
 class ContactTestCase(TestCase):
 
-    def create_contact(self, first_name="testing", last_name="testlast",
+    @staticmethod
+    def create_contact(first_name="testing", last_name="testlast",
                        email="test@isj.com", subject="Test the contact",
                        message="messageme"):
+        """ Create contact for tests """
         return Contact.objects.create(first_name=first_name, last_name=last_name,
                                       email=email, subject=subject,
                                       message=message)
 
-
     def test_create_contact(self):
+        """ Test if user is created correctly """
         obj = self.create_contact()
 
         self.assertTrue(isinstance(obj, Contact))
--- src/event/tests.py
+++ src/event/tests.py
@@ -2,24 +2,28 @@ from django.test import TestCase
 from event.models import Event
 from authentication.tests import ExtendedUser
 
+
 class EventTestCase(TestCase):
 
-    def create_user(self, first_name="test", last_name="forthewin",
+    @staticmethod
+    def create_user(first_name="test", last_name="forthewin",
                     username="kek", password="cevaparola", status=2):
+        """ Create user for tests """
         return ExtendedUser.objects.create_user(first_name=first_name,
                                                 last_name=last_name,
                                                 username=username,
                                                 password=password,
                                                 status=status)
 
-    def create_event(self, name="de test", text="ca sa fie"
-                     , address="Something",
+    def create_event(self, name="de test", text="ca sa fie", address="Something",
                      geolocation="4,20"):
+        """ Test test event """
         return Event.objects.create(name=name, text=text,
                                     address=address, geolocation=geolocation,
-                                    author = self.create_user())
+                                    author=self.create_user())
 
     def test_create_event(self):
+        """ Test if event is created correctly"""
         obj = self.create_event()
 
         self.assertTrue(isinstance(obj, Event))
