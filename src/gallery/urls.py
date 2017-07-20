cf04c14e65320e90b0f58319b5b8a5e6701e99b5
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-07-20 19:31:15 +0300

    I changed url for gallery

--- src/gallery/urls.py
+++ src/gallery/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^album', views.gallery, name='gallery'),
-    url(r'^gallery_img/(?P<slug>[^\.]+)/$', views.gallery_img, name='gallery'),
+    url(r'^gallery/(?P<slug>[^\.]+)/$', views.gallery_img, name='gallery'),
     url(r'^remove_gallery_photo', views.remove_gallery_photo, name='remove_gallery_photo'),
     url(r'^add_gallery', views.add_gallery, name='add_gallery'),
 
