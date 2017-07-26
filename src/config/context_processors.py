028360d308c385538d341acc98dde822f9392e93
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-07-26 16:32:23 +0300

    responsive img in text rich

--- src/config/context_processors.py
+++ src/config/context_processors.py
@@ -5,7 +5,7 @@ from subject.models import Subject
 def template_context(request):
 
     return {
-        'categories': Category.objects.all()[:7],
+        'categories': Category.objects.all()[:6],
         'categories_number':Category.objects.all().count(),
         'left_subjects': Subject.objects.all()
     }
