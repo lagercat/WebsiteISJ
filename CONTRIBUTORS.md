95b13513d21efc32ffa2ca264789f20f564d25c8
MalesSebastian <malessebastian@gmail.com>
2017-07-27 16:41:49 +0300

    License project to GPL3

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,5 @@
+Contributors, in alphabetical order.
+
+	Adrian-Ioan Gărovăț
+    Emanuel Covaci
+    Sebastian-Valeriu Maleș
--- src/utility/templatetags/material_form.py
+++ src/utility/templatetags/material_form.py
@@ -1,3 +1,19 @@
+# Copyright 2017 Adrian-Ioan Gărovăț, Emanuel Covaci, Sebastian-Valeriu Maleș
+#
+# This file is part of WebsiteISJ
+#
+# WebsiteISJ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WebsiteISJ is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WebsiteISJ.   If not, see <http://www.gnu.org/licenses/>.
 import os
 import re
 from collections import defaultdict
