e699a1145539ce351f51c717265f1dacf89b58aa
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-10-09 15:33:58 +0300

    registration app edu in progress

--- src/registration/views.py
+++ src/registration/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def primary_school(request):
+    return render(request,'registration/primary_all.html')
+
+def pre_school(request):
+    return render(request,'registration/pre-school_all.html')
