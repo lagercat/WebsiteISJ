2724ef4bf6a27dc6f551522f6ec71e99265efe40
MalesSebastian <malessebastian@gmail.com>
2017-11-17 19:56:44 +0200

    Extend size limit for email

--- src/school/models.py
+++ src/school/models.py
@@ -44,7 +44,7 @@ class School(CustomPermissionsMixin):
     name = models.CharField(max_length=100, null=True)
     telephone = PhoneNumberField(blank=True)
     fax = PhoneNumberField(blank=True)
-    email = models.EmailField(max_length=20, blank=True)
+    email = models.EmailField(max_length=50, blank=True)
     website = models.CharField(max_length=100, blank=True)
     address = map_fields.AddressField(max_length=200)
     geolocation = map_fields.GeoLocationField(max_length=100)
